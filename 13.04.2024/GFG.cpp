3b5f49e49c0253e25fc7f805b0d01b4b172bd760
keshavvijay092 <keshavvijay092@gmail.com>
2024-04-13 21:08:21 +0530

    this is the potd of 13th april 2024

---
+++ 13.04.2024/GFG.cpp
@@ -0,0 +1,17 @@
+  string s="";
+        for(int i=0;i<=31;i++){
+            if(x & (1<<i)){
+                s='1'+s;
+            }
+            else{
+                s='0'+s;
+            }
+        }
+        
+        long long ans=0;
+        for(int i=0;i<=31;i++){
+            if(s[i]=='1'){
+                ans+=pow(2,i);
+            }
+        }
+        return ans;
